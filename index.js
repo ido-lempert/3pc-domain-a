9d6646a1d024a1e72bc3416575394e17eefa65d0
ido-lempert <il.mrbit@gmail.com>
2024-02-01 19:09:42 +0200

    9

--- index.js
+++ index.js
@@ -5,15 +5,20 @@ const port = process.env.PORT || 3000;
 const cookieParser = require('cookie-parser');
 app.use(cookieParser());
 
-app.use(express.static('public'));
-
-app.get('/', (req, res) => {
+app.use((req,res, next)=>{
     res.cookie('X-Domain-A', `**${Math.floor(Math.random()*100000)}**`, {
         path: '/',
         sameSite: 'none',
         secure: true,
         maxAge: 24 * 60 * 60 * 1000
     });
+    next();
+});
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+
     res.send('Hello domain-a');
 })
 
